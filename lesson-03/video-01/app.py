e0e123f30977dbb4f76dcfbb654cbcc275d4b1a2
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2025-03-12 22:01:45 +0100

    todo app step 1

--- lesson-03/video-01/app.py
+++ lesson-03/video-01/app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    completed = db.Column(db.Boolean)
+
+
 @app.route('/')
 def index():
-    return 'Hello World!'
+    todo_list = Todo.query.all()
+    return render_template('base.html', todo_list=todo_list)
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
