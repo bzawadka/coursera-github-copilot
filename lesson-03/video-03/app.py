18ce865f9d02d52426222de7dab6a3b3fee9d7fe
Bartlomiej Zawadka <bartlomiej.zawadka@gmail.com>
2025-03-13 23:01:10 +0100

    todo app refactor 6

--- lesson-03/video-03/app.py
+++ lesson-03/video-03/app.py
@@ -6,6 +6,4 @@ from app.models.todo import db
 app = create_app()
 
 if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # Create database tables
     app.run(debug=True)
--- lesson-03/video-03/app/__init__.py
+++ lesson-03/video-03/app/__init__.py
@@ -3,7 +3,7 @@ from app.models.todo import db
 from config import Config
 from app.routes.todo_routes import todo_bp
 
-def create_app():
+def create_app() -> Flask:
     """Factory function to create and configure the Flask app."""
     app = Flask(__name__, template_folder='templates')
 
@@ -12,6 +12,9 @@ def create_app():
 
     # Initialize extensions
     db.init_app(app)
+
+    with app.app_context():
+        db.create_all()  # Create database tables
 
     # Register blueprints
     app.register_blueprint(todo_bp)
